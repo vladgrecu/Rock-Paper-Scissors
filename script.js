2355de49aed20a2e2922b0910987623b26ab48d4
vladgrecu <51508637+vladgrecu@users.noreply.github.com>
2019-08-13 23:50:58 +0300

    Added reset and some others

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const selected = document.getElementsByClassName('option');
 const posibilitati = ['rock', 'paper', 'scissors'];
 const joc = document.getElementById('joc');
-const h3 = document.getElementsByClassName('aiChoice')[0];
+const aiTitle = document.getElementsByClassName('aiChoice')[0];
+const resetBtn = document.querySelector('button');
+
+let playerWins = 0;
+let aiWins = 0;
 
 for (let i = 0; i < selected.length; i++){
     let option = selected[i];
@@ -17,15 +21,21 @@ function play(option){
     let aiPick = posibilitati[Math.floor(Math.random() * posibilitati.length)];
     console.log('AI: ', aiPick);
     if (playerPick === aiPick){
-        console.log("Remiza");
+        posteazaRezultat("Remiza");
     }
     else if((playerPick === 'rock' && aiPick ==='paper')||
             (playerPick === 'scissors' && aiPick === 'rock')||
             (playerPick === 'paper' && aiPick === 'scissors')){
-        console.log("You lose");
+        posteazaRezultat("You lose");
+        aiWins++;
+        document.getElementById("aiWins").innerHTML = aiWins;
+        console.log(aiWins);
     }
     else {
-        console.log("You Win");
+        posteazaRezultat("You Win");
+        playerWins++;
+        document.getElementById("playerWins").innerHTML = playerWins;
+        console.log(playerWins);
     }
     showAiPick(playerPick, aiPick);
 }
@@ -33,7 +43,7 @@ function showAiPick(player, ai){
     let alegere = document.createElement('div');
     alegere.classList.add("optionAI", ai);
     joc.appendChild(alegere);
-    h3.classList.remove('aiChoice');
+    aiTitle.classList.remove('aiChoice');
 }
 function disable() {
     for (let i = 0; i < selected.length; i++){
@@ -42,4 +52,21 @@ function disable() {
             option.classList.add("disable");
         }
     }
+}
+function posteazaRezultat(rez){
+    let rezultat = document.getElementById('rezultat');
+    rezultat.innerHTML = rez;
+}
+
+resetBtn.addEventListener("click", reset);
+
+function reset(){
+    for (let i = 0; i < selected.length; i++){
+        let option = selected[i];
+
+        option.classList.remove("selected");
+        option.classList.remove("optionAI");
+        option.classList.remove("disable");
+    }
+    joc.innerHTML = '';
 }
